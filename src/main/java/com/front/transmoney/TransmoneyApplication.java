6adc092b1a3111f7dd824c4c5cceab4eaac955c6
xnzale <bdnzale@gmail.com>
2019-10-02 11:40:27 +0000

    first

---
+++ src/main/java/com/front/transmoney/TransmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.front.transmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransmoneyApplication.class, args);
+	}
+
+}
